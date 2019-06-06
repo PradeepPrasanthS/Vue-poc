cc91b36bf4883fd79cc24d9d1f1c0d074dec470a
PradeepPrasanthS <pradeep.prasanth@diatoz.com>
2019-06-06 23:02:24 +0530

    Add all my files

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+        <p>Name: <input type="text"></p>
+        <p>Password: <input type="password"></p>
+        <button>Login</button>
+        
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
