fd56476f334be4a2608267d46dd4b9d40114f48a
PradeepPrasanthS <pradeep.prasanth@diatoz.com>
2019-06-06 23:50:01 +0530

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Dashboard></Dashboard>
+  <div>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Dashboard></Dashboard>
+    </div>
+    <div>
+      <Footer></Footer>
+    </div>
   </div>
+    
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Dashboard from './components/Dashboard.vue'
+import Footer from './components/Footer'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Dashboard
+    Dashboard,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="footer">
+            <p>Copyrights &copy; Pradeep Prasanth</p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.footer {
+    position: fixed;
+    /* left: 0; */
+    bottom: 0;
+    width: 100%;
+    background-color: #2c3e50;
+    color: white;
+    text-align: center;
+
+  
+}
+</style>
+
+
